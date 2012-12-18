ce7a01516b97b05a83ca5e7999aca7441aa7a369
Brad Janke <brad.janke@gmail.com>
2012-12-17 23:14:11 -0600

    Gets dimensions of the images

--- processor.py
+++ processor.py
@@ -1,7 +1,22 @@
-import os
+import os, re
 
-image_folder = 'images'
+image_dir = 'images'
 
-dirlist = os.listdir(image_folder)
+for root, dirs, files in os.walk(image_dir):
+    for f in files:
+        if (os.path.splitext(f)[1].strip() != '.jpg'): continue
+        out = os.popen('identify -format "%[fx:w] by %[fx:h] pixels" ' + os.path.join(root, f))
+        out = out.readline().strip()
 
-print dirlist
+        m = re.search('(^\d{1,4}) by (\d{1,4}) pixels$', out)
+        
+        if not m:
+            print 'no match for', f
+            continue
+
+        dims = tuple([m.group(1), m.group(2)])
+        
+        if (dims[0] > dims[1]):
+            print 'landscape'
+        else:
+            print 'portrait'
