37f006f60814329647ce63ff5113376fe1ec5104
Brad Janke <brad.janke@gmail.com>
2012-12-17 23:47:38 -0600

    The image processor works as it is supposed to. Need to add some configuration.

--- processor.py
+++ processor.py
@@ -3,9 +3,16 @@ import os, re
 image_dir = 'images'
 
 for root, dirs, files in os.walk(image_dir):
+    print 'in', root
     for f in files:
-        if (os.path.splitext(f)[1].strip() != '.jpg'): continue
-        out = os.popen('identify -format "%[fx:w] by %[fx:h] pixels" ' + os.path.join(root, f))
+        f = os.path.join(root, f)
+        split = os.path.splitext(f)
+
+        if (split[1].strip() != '.jpg' or re.search('_thumb$', split[0])):
+            print 'skipping thumb', f
+            continue
+
+        out = os.popen('identify -format "%[fx:w] by %[fx:h] pixels" ' + f)
         out = out.readline().strip()
 
         m = re.search('(^\d{1,4}) by (\d{1,4}) pixels$', out)
@@ -14,9 +21,8 @@ for root, dirs, files in os.walk(image_dir):
             print 'no match for', f
             continue
 
-        dims = tuple([m.group(1), m.group(2)])
-        
-        if (dims[0] > dims[1]):
-            print 'landscape'
-        else:
-            print 'portrait'
+        dims = tuple([m.group(1), m.group(2)])        
+        scale = '250' if dims[0] > dims[1] else 'x250'
+
+        out = os.popen('convert %s -scale "%s" %s' % (f, scale, split[0] + '_thumb' + split[1]))
+        print 'converted', f
