acb430e8904d8c03f7b18071efa4e152654e1a97
Brad Janke <brad.janke@gmail.com>
2012-12-21 10:02:19 -0600

    Updated api reference links on readme

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ Simple Image Viewer
 =================
 Todo.... this.
 
-Help
+API References
 ====
 1. http://www.imagemagick.org/script/command-line-tools.php
-2. http://www.imagemagick.org/script/identify.php
-3. http://www.imagemagick.org/script/command-line-options.php#scale
-4. http://www.imagemagick.org/script/command-line-processing.php#geometry
+2. http://boto.cloudhackers.com/en/latest/index.html
+3. http://docs.amazonwebservices.com/AWSJavaScriptSDK/latest/frames.html
+4. https://github.com/aws/aws-sdk-js
+5. http://expressjs.com/api.html
+6. https://github.com/visionmedia/jade#readme
--- imageprocessing/uploader.py
+++ imageprocessing/uploader.py
@@ -1,4 +1,4 @@
-import boto, os, re, uuid, time, hashlib
+import boto, os, re, uuid, time, hashlib, argparse
 from boto.exception import *
 from boto import dynamodb
 
@@ -6,49 +6,74 @@ bucket_name = 'com.bradjanke.photos'
 image_dir = 'images/output'
 dyno_key = 'dallasmarathon'
 
-conns3 = boto.connect_s3()
-b = conns3.create_bucket(bucket_name)
-
-def uploadImage(image, key):
+def uploadFile(b, image, key, folder=None):
     try:
-        print 'uploading', image
+        if folder: key = '%s/%s' % (folder, key)
 
         k = b.new_key(key)
+        k.set_metadata('Cache-Control', 'max-age=%d, public' % (86400 * 14))
+        k.set_metadata('Content-Type', 'image/jpg')
         k.set_contents_from_filename(image)
         k.set_acl('public-read')
-        return k.generate_url(0, query_auth=False)
+        print 'uploaded', image
     except S3ResponseError, e:
+        print 'FAILED', image
         print e.error_code
         print e.error_message
     except Exception, e:
+        print 'FAILED', image
         print e
-    return None
 
-for root, dirs, files in os.walk(image_dir):
-    print 'in', root
-    for fname in files:
-        f = os.path.join(root, fname)
-        split = os.path.splitext(f)
+def main():
+    parser = argparse.ArgumentParser(prog='uploader.py')
+    parser.add_argument(
+        '-bucket',
+        help='bucket to upload into',
+        metavar='name.of.bucket',
+        required=True)
+    parser.add_argument(
+        '-files', 
+        help='directory of files to upload',
+        metavar='/bar/dir',
+        required=True)
+    parser.add_argument(
+        '-s3folder', 
+        help='folder to place uploads into',
+        required=False)
 
-        if (split[1].strip() != '.jpg' or not re.search('(_full)$', split[0])):
-            print 'skipping', fname
-            continue
+    args = parser.parse_args()
+    bucket_name = args.bucket
+    image_dir = os.path.normpath(args.files)
+    dyno_key = args.s3folder
 
-        thumb = fname.replace('_full', '_thumb')
-        if not os.path.exists(os.path.join(root, thumb)):
-            print 'thumb does not exist', thumb
-            continue
+    conns3 = boto.connect_s3()
+    b = conns3.create_bucket(bucket_name)
 
-        out = os.popen('identify -format "%[EXIF:DateTime]" ' + f)
-        out = out.readline().strip()
-        m = re.search('^([0-9: ]+$)', out)
+    for root, dirs, files in os.walk(image_dir):
+        print 'in', root
+        for fname in files:
+            f = os.path.join(root, fname)
+            split = os.path.splitext(f)
 
-        if not m:
-            print 'no datetime for', f
-            continue
+            if (split[1].strip() != '.jpg' or not re.search('(_full)$', split[0])):
+                print 'skipping', fname
+                continue
 
-        full = uploadImage(f, dyno_key + fname)
-        thumb = uploadImage(os.path.join(root, thumb), dyno_key + thumb)
+            thumb = fname.replace('_full', '_thumb')
+            if not os.path.exists(os.path.join(root, thumb)):
+                print 'thumb does not exist', thumb
+                continue
 
-        if (full == None or thumb == None):
-            print 'full or thumb failed'
+            out = os.popen('identify -format "%[EXIF:DateTime]" ' + f)
+            out = out.readline().strip()
+            m = re.search('^([0-9: ]+$)', out)
+
+            if not m:
+                print 'no datetime for', f
+                continue
+
+            uploadFile(b, f, fname, dyno_key)
+            uploadFile(b, os.path.join(root, thumb), thumb, dyno_key)
+
+if __name__ == "__main__":
+    main()
